d5b47ea5d7510bea2cbbf00ccd917ff9212ff456
aravindinavolu <43317563+aravindinavolu@users.noreply.github.com>
2020-03-05 16:05:01 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,6 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    print("\n\n\n\n\n\n\n\n)
     return "                   Welcome to Tech Data!"
 
 if __name__ == "__main__":
